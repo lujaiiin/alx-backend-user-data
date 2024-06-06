025b4a63584d42b6efe999019b12d560d71379b8
lujain <lujain.com>
2024-06-06 19:49:50 +0200

    lala

--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python3
-""" Module of Index views
-"""
+"""modules"""
+
 from flask import jsonify, abort
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
-    """
+    """func"""
+
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
-    """
+    """fun"""
+
     from models.user import User
     stats = {}
     stats['users'] = User.count()
@@ -28,17 +24,13 @@ def stats() -> str:
 
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
-      - 401 error
-    """
+    """func"""
+
     abort(401)
 
 
 @app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> str:
-    """ GET /api/v1/forbidden
-    Return:
-      - 403 error
-    """
+    """fun"""
+
     abort(403)
